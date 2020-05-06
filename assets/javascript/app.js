a714235c84cf70d28e7d4745681391a71135f50e
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-05-05 22:28:17 -0400

    work to javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
 
-    
-}
+    const key = "7w8Zsws3Lz5IuLUemcGQkgK8c0Riv4Hz";
+    const q 
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + 
+
+
+
+    function displayMeme() {
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        })
+        .then(function(response) {
+
+            let results = Response.data;
+
+        })
+    }
+})
