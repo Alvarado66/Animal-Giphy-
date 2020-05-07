c16f17ff416bec52a6e0d74f0f1242f8bc508bff
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-05-07 07:57:30 -0400

    organized the app file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
 
     const key = "7w8Zsws3Lz5IuLUemcGQkgK8c0Riv4Hz";
-    let q = []
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + 
-    let 
-
-
-
-    }
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + ;
+    let cartoons = ["Bugs Bunny", "Daffy Duck", "Pokemon"] 
 
     
+
+
 })
