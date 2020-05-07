6ce2ed6a6150ce248e47ea63fc8267333e966f64
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-05-07 07:51:21 -0400

    slight css change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,12 @@
 $(document).ready(function() {
 
     const key = "7w8Zsws3Lz5IuLUemcGQkgK8c0Riv4Hz";
-    let q = 
+    let q = []
     var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + 
-    let vehicle = 
+    let 
 
 
 
-    function displayMeme() {
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        })
-        .then(function(response) {
-
-            let results = Response.data;
-
-        })
-    }
-
-    function renderButton() {
-        $("#newGifs").empty();
-
-        for (let i = 0; i < memes.length; i++) {
-            
-            let choice = $("<button>");
-            choice.addClass("img")
-            choice.attr("data-name"), 
-        }
-        
     }
 
     
