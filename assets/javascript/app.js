dd0cda64b2c78c83ab515b9747079e22d7359771
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-05-07 07:23:32 -0400

    added text input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     const key = "7w8Zsws3Lz5IuLUemcGQkgK8c0Riv4Hz";
-    const q 
+    let q = 
     var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + 
     let vehicle = 
 
