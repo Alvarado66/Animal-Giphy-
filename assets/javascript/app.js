f81ac151952a520bf497c33da9424ad6af6a6df0
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-05-08 13:44:24 -0400

    bug with var identifier

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,16 @@
-$(document).ready(function() {
-
-    const key = "7w8Zsws3Lz5IuLUemcGQkgK8c0Riv4Hz";
-    // my API Key
-    let queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + 
-    // the queryURL to direct user for GIFs
-    let q = [];
-    // what "keyphrase" the user types
-    const limit = 10;
-    // will limit how many GIFs to show up after click
-    let rating = [];
-    // the rating of each GIF
-    let cartoons = ["Bugs Bunny", "Woody", "Mickey Mouse"];
-    //  Basic Starter Buttons 
-
-
-
-
-
+$(document).ready() ({
+    // const key = "7w8Zsws3Lz5IuLUemcGQkgK8c0Riv4Hz";
+    // // my API Key
+    // let queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + 
+    // // the queryURL to direct user for GIFs
+    // // let q = [];
+    // // what "keyphrase" the user types
+    // // const limit = 10; 
+    // // will limit how many GIFs to show up after click
+    // // let rating = [];
+    // // the rating of each GIF
+    var cartoons = ["Bugs Bunny", "Woody", "Mickey Mouse",];
+    // //  Basic Starter Buttons 
 
     function renderButtons() {
 
@@ -27,34 +21,33 @@ $(document).ready(function() {
         for (var i = 0; i < cartoons.length; i++) {
 
 
-        var character = $("<button>");
+        var a = $("<button>");
 
-        character.addClass("newCartoon");
+        a.addClass("cartoon");
 
-        character.attr("data-name", cartoons[i]);
+        a.attr("data-name", cartoons[i]);
 
-        character.text(cartoons[i]);
+        a.text(cartoons[i]);
 
-        $("#buttons-view").append(cartoons);
+        $("#buttons-view").append(a);
         }
     }
 
 
-    $("#addCartoon").on("click", function(event) {
+    $("#add-cartoon").on("click", function(event) {
 
         event.preventDefault();
 
 
-        var newCharacter = $("#cartoons-input").val().trim();
+        var cartoon = $("#add-cartoon").val().trim();
 
-        cartoons.push(character);
+        cartoons.push(cartoon);
 
 
         renderButtons();
     });
 
     renderButtons();
-
 })
 
 
